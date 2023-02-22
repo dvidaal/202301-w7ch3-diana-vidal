3c00eda8c6785c3e8e0234bf8c755954bc04cd44
Diana Vidal <dvidaal@gmail.com>
2023-02-22 22:18:45 +0100

    Create Header component and styles

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import HeaderStyled from "./HeaderStyled";
+
+const Header = (): JSX.Element => {
+  return (
+    <HeaderStyled>
+      <h1>APRENDE CON MARIO PARA NO SER UNX VULGAR PROGRAMADXR</h1>
+    </HeaderStyled>
+  );
+};
+
+export default Header;
