68f957db88c4f38a743aff11a4b81871285687f5
Diana Vidal <dvidaal@gmail.com>
2023-02-22 22:20:28 +0100

    Create Favourties component and styles

---
+++ src/components/Favourites/FavouritesStyled.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const FavouritesStyled = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  .favExpressions {
+    border-color: black;
+    border-width: 1px;
+    border-style: solid;
+    padding: 3px;
+  }
+`;
+
+export default FavouritesStyled;
